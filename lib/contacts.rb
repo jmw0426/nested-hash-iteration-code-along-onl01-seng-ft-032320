d7007b39102988f6a47ad6183f3c60536c75741a
Joshua Williams <jmw0426@hotmail.com>
2020-03-31 01:27:39 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,17 +1,17 @@
 
   # This is the array we will be passing into the remove_strawberry method
-  # contacts = {
-  #   "Jon Snow" => {
-  #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  #   },
-  #   "Freddy Mercury" => {
-  #     name: "Freddy",
-  #     email: "freddy@mercury.com",
-  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  #   }
-  # }
+  contacts = {
+    "Jon Snow" => {
+      name: "Jon",
+      email: "jon_snow@thewall.we", 
+      favorite_ice_cream_flavors: ["chocolate", "vanilla"]
+    },
+    "Freddy Mercury" => {
+      name: "Freddy",
+      email: "freddy@mercury.com",
+      favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
+    }
+  }
   
 def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
@@ -20,7 +20,9 @@ contacts.each do |person, contact_details_hash|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
-    
+    end
+  end
+end
   
 
     
